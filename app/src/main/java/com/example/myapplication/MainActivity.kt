eab33456fad35dee736315c1039fb0706dc9fca5
rhesatsaqif23 <atstsaqif23@gmail.com>
2026-09-11 10:14:20 +0700

    feat: change name to second person

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             MyApplicationTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Orang Pertama",
+                        name = "Second Person",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
